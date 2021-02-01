e21d2c104197ac436a99b3db0d950e50681b714b
张玉鹏 <abelzyp@foxmail.com>
2021-02-01 14:52:10 +0800

    func(test): 测试psvm的args

---
+++ src/test/java/jvm/JvmTest.java
@@ -0,0 +1,13 @@
+package jvm;
+
+import java.util.Arrays;
+
+/**
+ * @author Zhang Yupeng
+ * @since 2021/2/1
+ */
+public class JvmTest {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
