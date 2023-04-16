8b1b5cf0a6cf471326ee49fd6ee355f4f8645c8d
张 <abelzyp@foxmail.com>
2023-04-16 23:28:41 +0800

    add test main

---
+++ src/main/java/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @author Zhang
+ * @since 2023/4/15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
