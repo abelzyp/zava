9600a9129d48491b104e84bd5c0681b4d573a223
张 <abelzyp@foxmail.com>
2023-04-18 21:00:52 +0800

    完善Java包

--- java/src/main/java/future/FutureExecutor.java
+++ java/src/main/java/future/FutureExecutor.java
@@ -1,10 +1,12 @@
 package future;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Abel
